b094e91aa3c2e5e34080ac1c79092a04df19dfdc
EmmanuelBarboza <barbozaprogra18@gmail.com>
2025-01-09 23:59:12 -0600

    Documentation
    -Added documentation to the code
    -Placed a timer for the animal respawn

--- Scenes/Animal/animal.gd
+++ Scenes/Animal/animal.gd
@@ -1,6 +1,7 @@
 class_name Animal
 extends RigidBody2D
 
+#Variables
 @onready var on_screen_notifier: VisibleOnScreenNotifier2D = $OnScreenNotifier
 
 # Called when the node enters the scene tree for the first time.
@@ -8,12 +9,16 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	pass
 
-
 func _on_screen_exited() -> void:
-	print("Despawning")
+	#Cuando el animal sale de la pantalla lo despawneamos
+	despawn()
+
+
+func despawn() -> void:
+	#Emitimos la señal de que el animal murio
+	SignalManager.on_animal_died.emit()
 	set_process(false)
 	queue_free()
--- Scenes/Level/level.gd
+++ Scenes/Level/level.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
-
+#Variables
 @onready var animal_start: Marker2D = $AnimalStart
+@onready var respawn_timer: Timer = $AnimalRespawn
 
+#Constantes
 const ANIMAL: PackedScene = preload("res://Scenes/Animal/animal.tscn")
 
 
 
 
+
 func _ready() -> void:
+	#Conectamos el nivel al signal manager, especificamente a la señal
+	#Que se emite cuando un Animal muere (despawnea)
+	SignalManager.on_animal_died.connect(_on_animal_died)
+	#Spawneamos un animal al inicio
 	spawn_animal()
 
 
@@ -16,7 +23,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _on_animal_died() -> void:
+	#Iniciamos un temporizador
+	respawn_timer.start()
+
+func _on_animal_respawn_timeout() -> void:
+	#Cuando el temporizador se le acaba el tiempo spawneamos un animal
+	spawn_animal()
+
 func spawn_animal() -> void:
+	#Instanciamos al animal
 	var new_animal = ANIMAL.instantiate()
+	#Colocamos su posicion
 	new_animal.global_position = animal_start.global_position
+	#Lo añadimos al arbol
 	self.add_child(new_animal)
